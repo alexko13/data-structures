8d853cf4d896cf6ab1632fb50b4fd3f64f0da067
Alex Ko <alexko@utexas.edu>
2016-01-15 11:37:03 -0700

    implemented the linked list data structure

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,40 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
+  //O(1)
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if(list.head === null){
+      list.head = newNode;
+    }
+    if(list.tail === null){
+      list.tail = newNode;
+    }
+    if(list.tail){
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
+  //O(1)
   list.removeHead = function(){
+    var currentHeadPointer = list.head.next;
+    var temp = list.head.value;
+    delete list.head;
+    list.head = currentHeadPointer;
+    return temp;
   };
 
+  //O(n)
   list.contains = function(target){
+    var currentNode = list.head;
+    while(currentNode !== null) {
+      if(currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
