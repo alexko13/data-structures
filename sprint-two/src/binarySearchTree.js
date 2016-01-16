15e4a8c5ce16cac22aed2a910ff8efa9443082f7
Alex Ko <alexko@utexas.edu>
2016-01-15 21:06:35 -0700

    implemented the binary search tree data structure

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,50 @@
 var BinarySearchTree = function(value){
+	var obj = Object.create(BinarySearchTree.prototype);
+	obj.value = value;
+	obj.left;
+	obj.right;
+	return obj;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+	if(this.value > value){
+		if(!this.left) {
+			this.left = BinarySearchTree(value);
+		} else {
+			this.left.insert(value);
+		}
+	} else {
+		if(!this.right) {
+			this.right = BinarySearchTree(value);
+		} else {
+			this.right.insert(value);
+		}
+	}
+};
+
+BinarySearchTree.prototype.contains = function(target){
+	if(this.value === target){
+		return true;
+	} else{
+		if(target < this.value && this.left){
+			return this.left.contains(target);
+		} else if(this.right) {
+			return this.right.contains(target);
+		}
+	}
+
+	return false;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callBack){
+	callBack(this.value);
+	if(this.left){
+		this.left.depthFirstLog(callBack);
+	}
+	if(this.right){
+		this.right.depthFirstLog(callBack);
+	}
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
