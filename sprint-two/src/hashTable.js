3b8d592f46530ef3839b1ddccd8467a673231365
Alex Ko <alexko@utexas.edu>
2016-01-15 17:03:20 -0700

    implemented the hash table data structure

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,24 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if(!this._storage.get(i)) {
+  	this._storage.set(i, {});
+  }
+  var bucket = this._storage.get(i);
+  bucket[k] = v;
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  return this._storage.get(i)[k];
 };
 
 HashTable.prototype.remove = function(k){
-
+	var i = getIndexBelowMaxForKey(k, this._limit);
+	var bucket = this._storage.get(i);
+	delete bucket[k];
+	//Replace line above with bucket[k] = null;
+	//to pass old spec where expect(hashTable.retrieve('Steven')).to.equal(null);
 };
 
 
